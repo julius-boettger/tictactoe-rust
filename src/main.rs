5d856e6ae0b5cd902f52190bb24114431d9323bc
julius-boettger <boettger.julius@gmail.com>
2023-07-26 21:41:18 +0200

    implemented winner detection on columns

--- src/main.rs
+++ src/main.rs
@@ -91,22 +91,49 @@ fn get_board_status(board: &Board) -> Status {
 
     /// returns the first field that won a row on the board (if present)
     fn winner_in_row(board: &Board) -> Option<&Field> {
-        // vector of unique elements of row
+        // vector containing all winners (of all rows)
         let winners = board.iter()
-            // map each row to someone_won(row)
+            // map each row to its potential winner
             .map(|row| someone_won(row.iter().collect_vec()))
             // filter out None's
             .filter(|option| option.is_some())
             .collect_vec();
         // return None if there were no winners
-        if winners.len() == 0 { return None }
+        if winners.len() == 0 { return None; }
         // return first winner
-        else { return winners[0] }
+        else { return winners[0]; }
     }
+
+    /// returns the first field that won a column on the board (if present)
+    fn winner_in_column(board: &Board) -> Option<&Field> {
+        // for all columns
+        for col_i in 0 .. BOARD_SIZE {
+            // get column from board
+            let mut column: [&Field; BOARD_SIZE] = [&Empty; BOARD_SIZE];
+            for row_i in 0 .. BOARD_SIZE {
+                column[row_i] = &board[row_i][col_i];
+            }
+            // get potential winner
+            let winner = someone_won(column.to_vec());
+            // return winner if present
+            if winner.is_some() {
+                return winner;
+            }
+        }
+        // return None if there were no winners
+        return None;
+    }
+
+    // TODO combine possible results into array for figuring out winner?
 
     let winner_in_row = winner_in_row(board);
     if winner_in_row.is_some() {
-        return Status::SomeoneWon(*winner_in_row.expect(""))
+        return Status::SomeoneWon(*winner_in_row.expect(""));
+    }
+
+    let winner_in_column = winner_in_column(board);
+    if winner_in_column.is_some() {
+        return Status::SomeoneWon(*winner_in_column.expect(""));
     }
 
     Status::StillPlaying
@@ -117,9 +144,9 @@ fn main() {
     let mut board: Board = [[Empty; BOARD_SIZE]; BOARD_SIZE];
 
     // make some moves
-    board[0][0] = Cross;
-    board[0][1] = Cross;
-    board[0][2] = Cross;
+    board[0][2] = Circle;
+    board[1][2] = Circle;
+    board[2][2] = Circle;
 
     print_board(&board);
     println!("{}", get_board_status(&board));
