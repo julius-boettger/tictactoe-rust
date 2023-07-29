971bab739bcbd877fb8bf54badfcf0633fae78e8
julius-boettger <boettger.julius@gmail.com>
2023-07-29 22:03:15 +0200

    wrote get_input helper function

--- src/main.rs
+++ src/main.rs
@@ -198,6 +198,19 @@ fn get_board_status(board: &Board) -> Status {
     Status::StillPlaying
 }
 
+/// get a line of user input through stdin after printing an info-text
+fn get_input(info: &str) -> String {
+    use std::io::{stdin, stdout, Write};
+    // print info text and flush the output
+    print!("{info}");
+    stdout().flush().expect("something went wrong when flushing stdout");
+    // read input
+    let mut buffer = String::new();
+    stdin().read_line(&mut buffer).expect("something went wrong reading stdin");
+    // return input without newline at end
+    buffer.trim_end().into()
+}
+
 fn main() {
     // new empty board
     let mut board: Board = [[Empty; BOARD_SIZE]; BOARD_SIZE];
