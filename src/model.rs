404959b72ce48ced8e402d4fc59f3cd6aab20274
julius-boettger <boettger.julius@gmail.com>
2023-09-11 23:03:25 +0200

    refactor

--- src/model.rs
+++ src/model.rs
@@ -1,17 +1,18 @@
 use crate::constants::*;
 
-pub type Line = [crate::model::Field; BOARD_SIZE];
-pub type Board = [Line; BOARD_SIZE];
-
 /// a field of a board which can be empty (None)
 /// or have some kind of symbol on it.
 /// a non-empty field can be considered a player.
 pub type Field = Option<char>;
+/// a line of a tictactoe board consisting of multiple Field's
+pub type Line = [Field; BOARD_SIZE];
+/// a tictactoe board consisting of multiple Line's
+pub type Board = [Line; BOARD_SIZE];
 
 /// current status of the game
 #[derive(Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Status {
     Draw,
-    SomeoneWon(char), 
-    StillPlaying
+    StillPlaying,
+    SomeoneWon(char)
 }
