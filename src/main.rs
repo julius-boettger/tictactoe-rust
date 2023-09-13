18d2eb34cf9bf11b3f9c49a85cff44b554da1634
julius-boettger <boettger.julius@gmail.com>
2023-09-13 09:50:21 +0200

    doc: add release compile command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,9 @@
  * - extract strings to own module
  * - clear terminal after every error message
  * - make BOARD_SIZE changeable at runtime, e.g. by wrapping game in a struct
+ * 
+ * RELEASE COMPILE
+ * cross build --release --target x86_64-unknown-linux-gnu --target aarch64-unknown-linux-gnu --target riscv64gc-unknown-linux-gnu --target x86_64-pc-windows-gnu
  */
 
 mod view;
